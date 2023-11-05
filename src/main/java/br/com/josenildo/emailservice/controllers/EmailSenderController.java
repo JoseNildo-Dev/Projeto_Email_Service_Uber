b7c2ad6d8d050db8bb90ab811eb0036869cbbf6e
José Nildo de Macedo Pereira <Jos Nildo de Macedo Pereira>
2023-11-04 23:44:48 -0300

    Add: Expor o serviço pelo mapeamento "/api/email"

---
+++ src/main/java/br/com/josenildo/emailservice/controllers/EmailSenderController.java
@@ -0,0 +1,39 @@
+package br.com.josenildo.emailservice.controllers;
+
+import br.com.josenildo.emailservice.application.EmailSenderService;
+import br.com.josenildo.emailservice.core.EmailRequest;
+import br.com.josenildo.emailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailService) {
+        this.emailSenderService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> emailService(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email send successefull");
+        } catch (EmailServiceException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
+    }
+}
+
+
+
+
+
